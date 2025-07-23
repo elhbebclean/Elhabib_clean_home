b2f5653cf00f1d6f58fd1980b030a532f37e2f52
elhbebclean <sseka2187@gmail.com>
2025-07-23 22:13:51 +0300

    Update service-worker.js

--- app/app/service-worker.js
+++ app/app/service-worker.js
@@ -1,14 +1,32 @@
-self.addEventListener('install', function(e) {
-  e.waitUntil(
-    caches.open('habib-clean').then(function(cache) {
-      return cache.addAll(['/', '/index.html']);
+const CACHE_NAME = "elhabib-clean-cache-v1";
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
     })
   );
 });
-self.addEventListener('fetch', function(e) {
-  e.respondWith(
-    caches.match(e.request).then(function(response) {
-      return response || fetch(e.request);
-    })
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames =>
+      Promise.all(
+        cacheNames.filter(name => name !== CACHE_NAME).map(name => caches.delete(name))
+      )
+    )
   );
 });
